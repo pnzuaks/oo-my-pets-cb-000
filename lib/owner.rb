8e28cd8b79ed909efbe7a5039824a1ae2dc84d21
Patricia Nykaza <pnykaza@calstatela.edu>
2018-07-21 14:28:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  attr_accessor :pets 
+  
+  @@all = []
+  
+  def initialize(species)
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species
+  end
+  
+  def self.all()
+    @@all
+  end
+  
+  def self.count()
+    @@all.count
+  end
+  
+  def self.reset_all()
+    @@all = []
+  end
+  
+  def say_species()
+    return "I am a #{@species}."
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def buy_fish(name)
+    pet_fish = Fish.new(name)
+    @pets[:fishes] << pet_fish
+  end
 end
